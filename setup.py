af276c76d92982c02ed373b6dd0d9daaeb8880ef
AnnelineD <anneline.daggelinckx@hotmail.com>
2022-10-27 15:54:26 +0200

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='LTL',
+    version='0.7.0',
+    packages=['ltl'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Anneline Daggelinckx',
+    author_email='',
+    description=''
+)
